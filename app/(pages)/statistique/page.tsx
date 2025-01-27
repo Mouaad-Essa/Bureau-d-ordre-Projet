298425e48abf7eb5cfe859c279cc38993ef10b39
Ayoub <ayoub-mhb@hotmail.fr>
2025-01-27 12:44:45 +0100

    Re-add all files to the branch

---
+++ app/(pages)/statistique/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Statistique = () => {
+  return <div>statistique</div>;
+};
+
+export default Statistique;
---
+++ components/nav-main.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { useState } from "react";
+import { ChevronRight, type LucideIcon } from "lucide-react";
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar";
+import { useRouter } from "next/navigation";
+
+export function NavMain({
+  items,
+  menuName,
+}: {
+  items: {
+    title: string;
+    url: string;
+    icon?: LucideIcon;
+    isActive?: boolean;
+    items?: {
+      title: string;
+      url: string;
+    }[];
+  }[];
+  menuName?: string;
+}) {
+  // Track the active menu item
+  const [activeMenu, setActiveMenu] = useState<string | null>(
+    items.find((item) => item.isActive)?.title || null
+  );
+
+  const router = useRouter();
+
+  const handleMenuClick = (title: string, url: string) => {
+    setActiveMenu(title);
+    router.push(url);
+  };
+
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>{menuName}</SidebarGroupLabel>
+      <SidebarMenu>
+        {items.map((item) => (
+          <Collapsible
+            key={item.title}
+            asChild
+            defaultOpen={activeMenu === item.title}
+            className="group/collapsible"
+          >
+            <SidebarMenuItem>
+              <CollapsibleTrigger asChild>
+                <SidebarMenuButton
+                  tooltip={item.title}
+                  className={`flex items-center gap-2 rounded-lg px-2 py-2 ${
+                    activeMenu === item.title
+                      ? "bg-[#333] text-[#FFF]" // Active state styles
+                      : "text-gray-700 hover:bg-gray-100 hover:text-gray-900" // Default and hover styles
+                  }`}
+                  onClick={() => handleMenuClick(item.title, item.url)} // Update active state
+                >
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                  {item.items && (
+                    <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                  )}
+                </SidebarMenuButton>
+              </CollapsibleTrigger>
+              {item.items && (
+                <CollapsibleContent>
+                  <SidebarMenuSub>
+                    {item.items.map((subItem) => (
+                      <SidebarMenuSubItem key={subItem.title}>
+                        <SidebarMenuSubButton asChild>
+                          <a href={subItem.url}>
+                            <span>{subItem.title}</span>
+                          </a>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                </CollapsibleContent>
+              )}
+            </SidebarMenuItem>
+          </Collapsible>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  );
+}
